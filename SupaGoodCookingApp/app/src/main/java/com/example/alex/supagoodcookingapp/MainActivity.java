231d1650ec68bdd0bb7dbf4ce610e945f4e37623
AlexTamulaitis <avt3@illinois.edu>
2018-12-02 17:19:06 -0600

    access gallery

--- SupaGoodCookingApp/app/src/main/java/com/example/alex/supagoodcookingapp/MainActivity.java
+++ SupaGoodCookingApp/app/src/main/java/com/example/alex/supagoodcookingapp/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.alex.supagoodcookingapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+    ImageView imageToUpload;
+    Button identifyImage;
+    EditText uploadImageName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+
+        identifyImage = (Button) findViewById(R.id.bUploadImage);
+
+        uploadImageName = (EditText) findViewById(R.id.etUploadName);
+
+        imageToUpload.setOnClickListener(this);
+        identifyImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageToUpload:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+            case R.id.identifyImage:
+                //api call
+                break;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
     }
 }
